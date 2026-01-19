5e02c18df70e9b91fea2139b7324ba2c8b14a1a4
sam <samuel.m.lavoie@gmail.com>
2026-01-18 23:48:41 -0500

    Added version, website, github and discord in logs

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import * as fs from 'fs';
+import * as path from 'path';
 
+const package_json = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'package.json')).toString('utf8'));
 const logger = new Logger();
 
-logger.log("| ██████╗ ██╗     ██╗████████╗███████╗██████╗ ██████╗  ██████╗ ██╗    ██╗███████╗███████╗██████╗ ");
-logger.log("| ██╔══██╗██║     ██║╚══██╔══╝╚══███╔╝██╔══██╗██╔══██╗██╔═══██╗██║    ██║██╔════╝██╔════╝██╔══██╗");
-logger.log("| ██████╔╝██║     ██║   ██║     ███╔╝ ██████╔╝██████╔╝██║   ██║██║ █╗ ██║███████╗█████╗  ██████╔╝");
-logger.log("| ██╔══██╗██║     ██║   ██║    ███╔╝  ██╔══██╗██╔══██╗██║   ██║██║███╗██║╚════██║██╔══╝  ██╔══██╗");
-logger.log("| ██████╔╝███████╗██║   ██║   ███████╗██████╔╝██║  ██║╚██████╔╝╚███╔███╔╝███████║███████╗██║  ██║");
-logger.log("| ╚═════╝ ╚══════╝╚═╝   ╚═╝   ╚══════╝╚═════╝ ╚═╝  ╚═╝ ╚═════╝  ╚══╝╚══╝ ╚══════╝╚══════╝╚═╝  ╚═╝");
+logger.log('#########################################################');
+logger.log(`# Version: ${package_json.version}`);
+logger.log(`# Website: https://blitzbrowser.com`);
+logger.log(`# Github : https://github.com/blitzbrowser/blitzbrowser`);
+logger.log(`# Discord: https://discord.com/invite/qZ3tCZJ2Ze`);
+logger.log('#########################################################');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
