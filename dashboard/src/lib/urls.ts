61bf433b6f892e73b3a5f7e948d4391a9ff2f83b
sam <samuel.m.lavoie@gmail.com>
2026-02-01 16:16:07 -0500

    Added dockerfile for dashboard and added it to docker-composeu

--- dashboard/src/lib/urls.ts
+++ dashboard/src/lib/urls.ts
@@ -1,5 +1,5 @@
-import { PUBLIC_API_URL } from "$env/static/public";
+import { env } from "$env/dynamic/public";
 
-export const api_url = new URL(PUBLIC_API_URL);
+export const api_url = new URL(env.PUBLIC_API_URL || 'http://localhost:9999');
 
 export const websocket_url = new URL(`${api_url.protocol === 'https' ? 'wss' : 'ws'}://${api_url.host}`)
